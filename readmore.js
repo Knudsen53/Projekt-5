af17d1e044d81e5d073217198afc72c892809902
Knudsen53 <frederikknudsen132@gmail.com>
2023-05-26 20:23:56 +0200

    d

--- readmore.js
+++ readmore.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const scrollDown = document.querySelector(".scroll-down");
   
     scrollDown.addEventListener("click", function() {
-      const section = document.getElementById("next-section");
+      const section = document.getElementById("next-section2");
       section.scrollIntoView({ behavior: "smooth" });
     });
   });
