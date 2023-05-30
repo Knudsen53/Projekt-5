54b47d6c0438fefd9be151c3469be3f1474a9457
Knudsen53 <frederikknudsen132@gmail.com>
2023-05-30 13:14:36 +0200

    d

---
+++ pages/faq/questions.js
@@ -0,0 +1,43 @@
+const nameInput = document.getElementById('name-input');
+    const surnameInput = document.getElementById('surname-input');
+    const emailInput = document.getElementById('email-input');
+    const topicInput = document.getElementById('topic-input');
+    const descriptionInput = document.getElementById('description-input');
+    const contactCheckbox = document.getElementById('contact-checkbox');
+    const sendButton = document.getElementById('send-button');
+
+    // Event listener for the Send button
+    sendButton.addEventListener('click', function() {
+      // Retrieve the values from the input fields
+      const name = nameInput.value;
+      const surname = surnameInput.value;
+      const email = emailInput.value;
+      const topic = topicInput.value;
+      const description = descriptionInput.value;
+      const allowContact = contactCheckbox.checked;
+
+      // Perform validation if needed
+
+      // Create an object with the form data
+      const formData = {
+        name: name,
+        surname: surname,
+        email: email,
+        topic: topic,
+        description: description,
+        allowContact: allowContact
+      };
+
+      // Perform further operations with the form data, such as sending it to a server
+
+      // Clear the input fields
+      nameInput.value = '';
+      surnameInput.value = '';
+      emailInput.value = '';
+      topicInput.value = '';
+      descriptionInput.value = '';
+      contactCheckbox.checked = false;
+
+      // Display a confirmation message or perform any other desired actions
+      alert('Form submitted successfully!');
+    });
