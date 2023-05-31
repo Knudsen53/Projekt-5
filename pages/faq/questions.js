59f3111b2920566e6d8237b0d9fc99a4081624e9
rebekah1712 <rebekah.de.nully@gmail.com>
2023-05-31 10:43:20 +0200

    Merge branch 'main' of https://github.com/Knudsen53/Projekt-5

--- pages/faq/questions.js
+++ pages/faq/questions.js
@@ -37,7 +37,21 @@ function handleSubmit(event) { //laver en funktion haldet handlesubmit, den tage
   displaySubmissions();      //Vi kalder funktionen displaysubmissions
 }
 
+function displaySubmissions() {     //funktion displaysubmissions laves
+  const submissionsList = document.getElementById('submissions-list');  //henter elementet i dommen med id 'submissions-list' og gemmer i konstanten submissionsList
+  submissionsList.innerHTML = '';                                               //sætter indholdet af submissionslist til tom
 
+  if (formSubmissions.length === 0) {     //Tjekker om denne statement er sand: hvis længden af formsubmissions er af typen og værdien 0 (=== sammenligningsoperator)
+    submissionsList.innerHTML = 'No submissions yet.';            //hvis den statement er sand, så sættes 'No submissions yet.' ind i submissionslist html
+  } else {                                                        //Hvis den statement ikke er sand:
+    for (let i = 0; i < formSubmissions.length; i++) {            //Så køres et loop: overall looper igennem længden formsubmissions fra index plads 0 og efter hver gennemgang af loopet tilføj 1 til i.
+      const submission = formSubmissions[i];                       //når loopet kører så gemmes værdien på plads [i] i konstanten submission. [i] starter på 0, og for hver gang vi går igennem loopet tilføjes der 1 til i. anden gennemgang er 1, tredje gennemgang er 2. Den kører først hele blokken fra linje 48 til 52 før [i] forstørres.
+      const listItem = document.createElement('li');        //laver et li element og gemmer referencen til det i konstanten listitem
+      listItem.textContent = `${submission.navn} ${submission.efternavn}: ${submission.emne}`; // Basically står der: document.createElement('li').textcontent - som siger at hvad end der står i elementet li laves til text og så sætter vi tekst i det element som hentes fra formsubmissions   ${submission.navn} henter navn fra formsubmissions (på linje 48 er formsubmissions med data gemt i en variabel kaldet submission)
+      submissionsList.appendChild(listItem);            //giv elementet 'submissions-list' et child som er det vi lavede på linje 50.
+    }
+  }
+}
 
 function startTimer() {
   const resetTime = 3;
